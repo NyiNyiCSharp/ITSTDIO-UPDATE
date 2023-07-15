edbac632273fb31f3ca27b29eb25120096f320d8
arkar543 <134772466+arkar543@users.noreply.github.com>
2023-07-15 14:35:06 +0630

    -

--- Models/ViewModels/BrandViewModel - Copy.cs
+++ Models/ViewModels/BrandViewModel - Copy.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace ITSTDIO_UPDATE_.Models.ViewModels
 {
-    public class BrandViewModel
+    public class BrandViewModelNew
     {
        
         public string Id { get; set; }
